4484406aa66f92bf15a3f291be013c0e82a44233
Guilherme <guiifernandes12042001@gmail.com>
2022-08-05 17:36:33 -0300

    farmacia beckend

---
+++ Farmacia/src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,55 @@
+package com.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max=255)
+    private String nome;
+
+    @NotBlank
+    @Size(max=255)
+    private String cor;
+
+
+    //getters and setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+}
